8f7cac929b42f8c09b4a116c7851d72954e1fdca
PratikNanda <pratik.nanda99@gmail.com>
2026-04-27 23:49:59 +0530

    Day 1 of Learning Pandas

---
+++ Pandas/01_Start_using_Pandas_in_2_minutes.py
@@ -0,0 +1,12 @@
+# Pandas = Panel Data
+# 
+# 
+# Work with series and DataFrames
+
+# Series (1D) --> single column; DataFrames --> Tables (2D) (Multi Columns)
+# 
+import pandas as pd
+
+print(pd.__version__)
+
+
