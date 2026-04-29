737cf1102d0c2dba4d5864af7fe46f06bf7d4455
Pratik Nanda <pratik.nanda99@gmail.com>
2026-04-30 01:00:25 +0530

    Filtering

---
+++ Pandas/06_Filtering_in_Pandas_is_easy.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+df = pd.read_csv("data.csv")
+
+# Filtering = Keeping the rows that match a condition
+
+tall_pokemon = df[df["Height"] > 2]
+heavy_pokemon = df[df["Weight"] > 100]
+legendary_pokemon = df[df["Legendary"] == 1]
+
+# OR 
+
+water_pokemon = df[(df["Type1"] == "Water") |
+                   (df["Type2"] == "Water")]
+
+# AND
+ff_pokemon = df[(df["Type1"] == "Fire") & (df["Type2"] == "Flying")]
+
+
+# print(tall_pokemon)
+# print(heavy_pokemon)
+# print(legendary_pokemon)
+# print(water_pokemon)
+print(ff_pokemon)
