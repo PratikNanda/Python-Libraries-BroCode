69dbb7011ded3e98dd38bb7d41c18aa9e85987ac
Pratik Nanda <pratik.nanda99@gmail.com>
2026-05-01 10:19:24 +0530

    started numpy

---
+++ Numpy/01_Start using NumPy in 5 minutes! 🔢.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# print(np.__version__)
+
+
+# my_list = [1, 2, 3, 4]
+# print(my_list * 2) # traditional list output --> [1, 2, 3, 4, 1, 2, 3, 4] 
+
+
+array = np.array([1, 2, 3, 4])
+print(array) # output --> [1 2 3 4]
+print(array * 2) # output --> [2 4 6 8]
+
+# print(type(array))
